962f7d71fe14d00ccfbc221e7c5c3723f2ac1255
Denis Mitlenko <softonka@gmail.com>
2024-01-02 01:19:07 +0200

    Add set_by_dot_path and get_by_dot_path utility functions

--- trackr/core/utility.py
+++ trackr/core/utility.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 
 
 def has_keys(d: dict, keys: list) -> bool:
@@ -13,38 +14,20 @@ def error_exit(logger=None, message='unknown error'):
     exit(1)
 
 
-def get_by_dot_path(d: dict, path: str, default=None):
-    if not isinstance(path, str):
-        raise ValueError('path must be a string')
+def set_by_dot_path(d: dict, path: str, value: Any) -> None:
+    """Set a value in a dictionary by a dot path. Example:
+    >>> d = {}  # Create an empty dictionary
+    >>> set_by_dot_path(d, "a.b.c", 1)  # Set the value 1 at a.b.c
+    >>> d
+    {'a': {'b': {'c': 1}}}
 
-    if not isinstance(d, dict):
-        raise ValueError('d must be a dict')
+    Args:
+        d (dict): The dictionary.
+        path (str): The dot path.
+        value (Any): The value.
+    """
 
-    if not path:
-        return d
-
-    keys = path.split('.')
-
-    for key in keys:
-        if key not in d:
-            return default
-
-        d = d[key]
-
-    return d
-
-
-def set_by_dot_path(d: dict, path: str, value):
-    if not isinstance(path, str):
-        raise ValueError('path must be a string')
-
-    if not isinstance(d, dict):
-        raise ValueError('d must be a dict')
-
-    if not path:
-        return d
-
-    keys = path.split('.')
+    keys = path.split(".")
 
     for key in keys[:-1]:
         if key not in d:
@@ -53,3 +36,28 @@ def set_by_dot_path(d: dict, path: str, value):
         d = d[key]
 
     d[keys[-1]] = value
+
+
+def get_by_dot_path(d: dict, path: str) -> Any:
+    """Get a value in a dictionary by a dot path. Example:
+    >>> d = {"a": {"b": {"c": 1}}}  # Create a dictionary
+    >>> get_by_dot_path(d, "a.b.c")  # Get the value at a.b.c
+    1
+
+    Args:
+        d (dict): The dictionary.
+        path (str): The dot path.
+
+    Returns:
+        Any: The value.
+    """
+
+    keys = path.split(".")
+
+    for key in keys:
+        if key not in d:
+            return None
+
+        d = d[key]
+
+    return d
