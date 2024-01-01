a7a1857fa13f5ae0ae6a362cf04111dff56f309f
Denis Mitlenko <softonka@gmail.com>
2024-01-01 23:00:13 +0200

    Add utility functions for accessing nested dictionary values

--- trackr/core/utility.py
+++ trackr/core/utility.py
@@ -11,3 +11,45 @@ def error_exit(logger=None, message='unknown error'):
 
     logger.error(message)
     exit(1)
+
+
+def get_by_dot_path(d: dict, path: str, default=None):
+    if not isinstance(path, str):
+        raise ValueError('path must be a string')
+
+    if not isinstance(d, dict):
+        raise ValueError('d must be a dict')
+
+    if not path:
+        return d
+
+    keys = path.split('.')
+
+    for key in keys:
+        if key not in d:
+            return default
+
+        d = d[key]
+
+    return d
+
+
+def set_by_dot_path(d: dict, path: str, value):
+    if not isinstance(path, str):
+        raise ValueError('path must be a string')
+
+    if not isinstance(d, dict):
+        raise ValueError('d must be a dict')
+
+    if not path:
+        return d
+
+    keys = path.split('.')
+
+    for key in keys[:-1]:
+        if key not in d:
+            d[key] = {}
+
+        d = d[key]
+
+    d[keys[-1]] = value
